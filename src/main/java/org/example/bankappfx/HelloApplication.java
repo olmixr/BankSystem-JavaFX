eb965227dbe0c15b19e8d4552c06da33f67cb25d
olmix <goldgeimkc@gmail.com>
2026-04-28 20:28:08 +0300

    addTextButtonVbox

--- src/main/java/org/example/bankappfx/HelloApplication.java
+++ src/main/java/org/example/bankappfx/HelloApplication.java
@@ -1,8 +1,11 @@
 package org.example.bankappfx;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,13 +13,33 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Text BankAccountText = new Text("BANK ACCOUNT APP");
+
+        Button CreateAccount = new Button("Create account");
+        Button DeleteAccount = new Button("Delete account");
+        Button DepositAccount = new Button("Deposit money");
+        Button WithDrawAccount = new Button("Withdraw money");
+        Button TransferMoneyAccount = new Button("Transfer from - to");
+        Button ShowAllAccounts = new Button("Show all accounts");
+        Button EndMonth = new Button("End of month");
+        Button SaveDataUsers = new Button("Save data Users");
+        Button GetDataUsers = new Button("Get data Users");
+        Button SearchAccount = new Button("Search account");
+        Button AutoSaveData = new Button("Auto save data"/* Тут должно быть True or False значение*/);
+        Button AutoLoadData = new Button("Auto load data"/* Тут должно быть True or False значение попозже сделать*/);
+        Button Exit = new Button("Exit");
+
+
+        VBox vbox = new VBox(10);
+        vbox.setAlignment(Pos.CENTER);
+        vbox.getChildren().addAll(BankAccountText,CreateAccount,DeleteAccount,DepositAccount,WithDrawAccount,TransferMoneyAccount,ShowAllAccounts,EndMonth,SaveDataUsers,GetDataUsers,SearchAccount,AutoSaveData,AutoLoadData);
+
+        Scene scene = new Scene(vbox,400,600);
+        stage.setTitle("1");
         stage.setScene(scene);
         stage.show();
     }
-
+    
     public static void main(String[] args) {
         launch();
     }
